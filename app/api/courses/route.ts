dce5aa82e276ef7057bf6031f49180d4f825f989
Emran-Y <emranyonas602@gmail.com>
2024-11-15 12:15:55 +0300

    feat:courseid page onpogress, editing title ui finished,patch api for course title finished and integrated

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse, NextRequest } from "next/server";
 
@@ -5,8 +6,21 @@ export async function POST(req: NextRequest) {
   try {
     const { userId } = getAuth(req);
     const {title} = await req.json();
+
+    if(!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.create({data:{
+      title,
+      userId
+    }})
+
+    return NextResponse.json(course);
   } catch (e) {
-    console.log("[COURSES", e);
+    console.log("[COURSES]", e);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
+
+
